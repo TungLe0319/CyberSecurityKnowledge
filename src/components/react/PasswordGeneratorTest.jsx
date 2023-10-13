28b173312f9ab65fffd52e9be8b32d31d34785ad
Tung N Le <tung.le0319@gmail.com>
2023-10-13 12:33:31 -0600

    created password generator

--- src/components/react/PasswordGeneratorTest.jsx
+++ src/components/react/PasswordGeneratorTest.jsx
@@ -15,7 +15,7 @@ class PasswordGeneratorTest extends Component {
   generatePassword(length, includeSpecialChars, includeNumbers) {
     let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     if (includeSpecialChars) {
-      charset += '!@#$%^&*'
+      charset += '!@#$%^&*()-_+.:;[]'
     }
     if (includeNumbers) {
       charset += '0123456789'
@@ -65,20 +65,50 @@ class PasswordGeneratorTest extends Component {
     alert(textToCopy)
   }
 
+  highlightNumbers = (text) => {
+    return text.replace(/\d+/g, (match) => `<span class=" font-semi-bold text-red-400">${match}</span>`)
+  }
   render() {
     return (
       <div className="mt-20 flex  flex-col items-center justify-center ">
-        <strong>Generated Password:</strong>
-        <div>
-          <div className="wrap flex w-1/2 flex-wrap p-1 text-4xl text-white">{this.state.password}</div>
+        <div className="rounded bg-slate-700 px-32">
+          <div
+            className="wrap-words text-center text-4xl text-white"
+            dangerouslySetInnerHTML={{ __html: this.highlightNumbers(this.state.password) }}
+          />
+
+          <div className="mb-3 flex">
+            <div className="flex items-center space-x-2">
+              {this.state.password.length >= 9 ? (
+                <>
+                  <img
+                    className="w-10"
+                    src="https://cdn-icons-png.flaticon.com/128/3643/3643948.png"
+                    alt="Strong Password"
+                  />
+                  <p className="font-semibold text-green-400">Strong Password</p>
+                </>
+              ) : (
+                <>
+                  <img
+                    className="w-10"
+                    src="https://cdn-icons-png.flaticon.com/128/7855/7855537.png"
+                    alt="Weak Password"
+                  />
+                  <p className="font-semibold text-orange-400">Moderate Password</p>
+                </>
+              )}
+            </div>
+          </div>
         </div>
+
         <div>
           <label>Password Length: </label>
           <input
             type="range"
             className="h-2 w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
             min="8"
-            max="60"
+            max="50"
             step="1"
             value={this.state.passwordLength}
             onChange={(e) => this.handlePasswordLengthChange(e.target.value)}
