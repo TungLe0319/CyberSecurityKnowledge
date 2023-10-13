0eecea42713258649786fb2eb48690411724ec86
Tung N Le <tung.le0319@gmail.com>
2023-10-13 11:37:53 -0600

    rtung

--- src/components/react/PasswordGeneratorTest.jsx
+++ src/components/react/PasswordGeneratorTest.jsx
@@ -1,88 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class PasswordGeneratorTest extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       password: this.generatePassword(8, true, true), // Generate a default password with length 8, including special characters and numbers
       passwordLength: 8,
       includeSpecialChars: true,
       includeNumbers: true,
-    };
+    }
   }
 
   generatePassword(length, includeSpecialChars, includeNumbers) {
-    let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     if (includeSpecialChars) {
-      charset += '!@#$%^&*';
+      charset += '!@#$%^&*'
     }
     if (includeNumbers) {
-      charset += '0123456789';
+      charset += '0123456789'
     }
 
-    let password = '';
+    let password = ''
     for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset[randomIndex];
+      const randomIndex = Math.floor(Math.random() * charset.length)
+      password += charset[randomIndex]
     }
-    return password;
+    return password
   }
 
   handlePasswordLengthChange = (newLength) => {
-    const newPassword = this.generatePassword(
-      newLength,
-      this.state.includeSpecialChars,
-      this.state.includeNumbers
-    );
-    this.setState({ password: newPassword, passwordLength: newLength });
-  };
+    const newPassword = this.generatePassword(newLength, this.state.includeSpecialChars, this.state.includeNumbers)
+    this.setState({ password: newPassword, passwordLength: newLength })
+  }
 
   handleSpecialCharsChange = () => {
     const newPassword = this.generatePassword(
       this.state.passwordLength,
       !this.state.includeSpecialChars,
       this.state.includeNumbers
-    );
-    this.setState({ includeSpecialChars: !this.state.includeSpecialChars, password: newPassword });
-  };
+    )
+    this.setState({ includeSpecialChars: !this.state.includeSpecialChars, password: newPassword })
+  }
 
   handleNumbersChange = () => {
     const newPassword = this.generatePassword(
       this.state.passwordLength,
       this.state.includeSpecialChars,
       !this.state.includeNumbers
-    );
-    this.setState({ includeNumbers: !this.state.includeNumbers, password: newPassword });
-  };
+    )
+    this.setState({ includeNumbers: !this.state.includeNumbers, password: newPassword })
+  }
 
+  copyToClipboard = () => {
+    const textToCopy = this.state.password
 
-    copyToClipboard= () => {
-    const textToCopy =  this.state.password
+    const textArea = document.createElement('textarea')
+    textArea.value = textToCopy
+    document.body.appendChild(textArea)
+    textArea.select()
+    document.execCommand('copy')
+    document.body.removeChild(textArea)
 
-    const textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
-
-    alert( textToCopy);
+    alert(textToCopy)
   }
 
   render() {
     return (
-      <div className="flex justify-center  items-center flex-col mt-20 ">
+      <div className="mt-20 flex  flex-col items-center justify-center ">
+        <strong>Generated Password:</strong>
         <div>
-          <strong>Generated Password:</strong>
-          <div className="text-white w-1/2 p-1 text-4xl flex flex-wrap wrap">{this.state.password}</div>
+          <div className="wrap flex w-1/2 flex-wrap p-1 text-4xl text-white">{this.state.password}</div>
         </div>
         <div>
           <label>Password Length: </label>
           <input
             type="range"
+            className="h-2 w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
             min="8"
-            max="030"
+            max="60"
             step="1"
             value={this.state.passwordLength}
             onChange={(e) => this.handlePasswordLengthChange(e.target.value)}
@@ -92,29 +88,34 @@ class PasswordGeneratorTest extends Component {
         <div>
           <label>
             <input
+              className=""
               type="checkbox"
               checked={this.state.includeSpecialChars}
               onChange={this.handleSpecialCharsChange}
-            /> Include Special Characters
+            />{' '}
+            Include Special Characters
           </label>
         </div>
         <div>
           <label>
             <input
+              className=""
               type="checkbox"
               checked={this.state.includeNumbers}
               onChange={this.handleNumbersChange}
-            /> Include Numbers
+            />{' '}
+            Include Numbers
           </label>
         </div>
 
         <div className="">
-
-          <button onClick={ this.copyToClipboard}></button>
+          <button className=" mt-4 hover:shadow-md hover:shadow-slate-600" onClick={this.copyToClipboard}>
+            Copy to Clipboard
+          </button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default PasswordGeneratorTest;
+export default PasswordGeneratorTest
